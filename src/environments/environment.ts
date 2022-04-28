5b5fc320a5c45d7125ac34b85d9ad0c6a2bdf85c
giodrn91 <gdcruz1991@gmail.com>
2022-04-27 19:40:57 -0600

    Start the project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'tryproject-95d11',
+    appId: '1:949464692405:web:3f86e7dcdf8db76046f0fc',
+    storageBucket: 'tryproject-95d11.appspot.com',
+    apiKey: 'AIzaSyBlf5iReubeRd1Tw0gaY96hdF0kxOJQihs',
+    authDomain: 'tryproject-95d11.firebaseapp.com',
+    messagingSenderId: '949464692405',
+  },
   production: false
 };
 
